f6e11d49ed1344c0d0b0a7b70e5a6ede067a9db6
Jis van Overschot <99040091@mydavinci.nl>
2017-04-11 11:57:03 +0200

    +fav icon

--- view/templates/header.php
+++ view/templates/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 	<head>
-		<title>Verjaardagskalender</title>
+		<title>Hospital</title>
 		<!--Import Google Icon Font-->
 		<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<!--Import materialize.css-->
@@ -10,6 +10,7 @@
         <link type="text/css" rel="stylesheet" href="<?= URL . '/public/css/style.css' ?>">
         <!--Let browser know website is optimized for mobile-->
       	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="icon" href="<?= URL . '/public/img/hospital-fav.png' ?>" type="image/png" sizes="24x24">
 	</head>
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
